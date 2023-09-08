ff22a9f39e0a8e34b34230f300249dde42883646
Karyyem <125399686+Karyyem@users.noreply.github.com>
2023-09-08 22:04:29 +0300

    Add files via upload
    Upload DIO and External Peripheral drivers

---
+++ external_peripherals.h
@@ -0,0 +1,40 @@
+/*
+ * leds.h
+ *
+ *  Created on: Sep 7, 2023
+ *      Author: Eng.Abdelrhman
+ */
+
+#ifndef EXTERNAL_PERIPHERALS_H_
+#define EXTERNAL_PERIPHERALS_H_
+
+#define RED_LED_PORT           DIO_PORTD
+#define RED_LED_PIN            DIO_PIN5
+
+#define GREEN_LED_PORT         DIO_PORTD
+#define GREEN_LED_PIN          DIO_PIN4
+
+#define BUZZER_PORT            DIO_PORTD
+#define BUZZER_PIN             DIO_PIN3
+
+#define RELAY_PORT             DIO_PORTB
+#define RELAY_PIN              DIO_PIN4
+
+void RELAY_voidInit(void);
+void LEDS_voidInit(void);
+void BUZZER_voidInit(void);
+
+void openRELAY(void);
+void closeRELAY(void);
+void useBuzzer(u8 toggleNumber);
+void openBuzzer(void);
+void closeBuzzer(void);
+
+void GreenLedON(void);
+void GreenLedOFF(void);
+void RedLedBlink();
+void RedLedOFF(void);
+void RedLedON(void);
+
+
+#endif /* EXTERNAL_PERIPHERALS_H_ */
