46002015b94c617224d1c0cddbb260ec26712d17
abdallah ibrahem <116827392+abdo888888888@users.noreply.github.com>
2023-09-08 21:03:53 +0300

    Add files via upload

---
+++ GIE.h
@@ -0,0 +1,18 @@
+/*
+ * GIE.h
+ *
+ *  Created on: Aug 16, 2023
+ *      Author: abdel
+ */
+
+#ifndef GIE_H_
+#define GIE_H_
+
+//SREG register bit 7
+#define SREG_I 7
+
+void GIE_Enable(void);
+void GIE_Disable(void);
+
+
+#endif /* GIE_H_ */
