6592ddb56b6fe4ce5ead370ed36c2f3490f02760
Abdelrhman <abdelrhmanhamdy023@gmail.com>
2023-09-08 22:25:49 +0300

    upload main Function

--- main.c
+++ main.c
@@ -27,6 +27,8 @@ void EXTI_ISR(void);
 void TIMER2_ISR(void);
 void systemFailure(void);
 void printCurrentTemp(void);
+void main_Function(void);
+
 
 u16 desiredTemp = 25; /*represent desired temp on the system*/
 u16 ADC_currentTemp; /*variable to take sensor readings*/
@@ -68,5 +70,115 @@ void main(){
 	}
 
 	while(1){
+		main_Function();
+	}
+}
+
+void main_Function(void){
+	u8 ADC_prevTemp = ADC_currentTemp; //record last temperature
+	/*take new readings*/
+	ADC_currentTemp=ADC_Read(0);
+	ADC_currentTemp = getTemp(ADC_currentTemp);
+
+	if(desiredTemp > ADC_currentTemp ){
+		Motor_speedUp(); //take an action by increasing motor speed
+		while(1){
+			if(desiredTemp <= ADC_currentTemp){
+				printCurrentTemp();
+				Motor_stop(); //stop motor as the desired temp had been reached
+				break;
+			}
+			/*if the temp changed but not reached desired temp yet*/
+			if(ADC_currentTemp != ADC_prevTemp){
+				printCurrentTemp();
+
+				if(ADC_currentTemp > UPPER_LIMIT){
+					systemFailure(); //system goes into failure
+					break;
+				}
+				Motor_speedUp(); //since desired temp had not been reached yet, then take an action
+			}
+			//take the sensor new readings
+			ADC_prevTemp = ADC_currentTemp;
+			ADC_currentTemp=ADC_Read(0);
+			ADC_currentTemp = getTemp(ADC_currentTemp);
+		}
+	}else if(desiredTemp < ADC_currentTemp){
+		u8 flag = 0;     //to indicate that the motor is rotating anti clock-wise
+		Motor_speedDown(); //decrease motor speed
+		//check if there is a need to reverse motor direction to reach desired temperature
+		if(OCR0 == 0){
+			flag = 1;
+			Motor_RotateAntiClockWise();
+			Motor_speedUp(); //increase the speed at the reverse direction
+		}
+
+		while(1){
+
+			if(desiredTemp >= ADC_currentTemp){
+				printCurrentTemp();
+				Motor_stop();
+				Motor_RotateClockWise(); //re-adjust rotation direction
+				break;
+			}
+			if(ADC_currentTemp != ADC_prevTemp){
+				printCurrentTemp();
+				if(ADC_currentTemp > UPPER_LIMIT){
+					systemFailure();
+					break;
+				}
+				if(flag){
+					//speed up in the anti-clockwise direction
+					Motor_speedUp();
+				}else{
+					Motor_speedDown();
+					if(OCR0 == 0){
+						flag = 1;
+						Motor_RotateAntiClockWise();
+					}
+				}
+			}
+			//take sensor readings
+			ADC_prevTemp = ADC_currentTemp;
+			ADC_currentTemp=ADC_Read(0);
+			ADC_currentTemp = getTemp(ADC_currentTemp);
+		}
+	}else{
+		Motor_stop(); //no action if temperatures are equal
+	}
+}
+
+
+void EXTI_ISR(void){
+	// Initialize UART to enable it only by the push button
+	USART_Init();
+
+	// Receive the string
+	u8 *data = "";
+	USART_ReceiveString(data);
+
+	//check validity of data
+	u8 i = 0; //iterator
+	while(data[i]){
+		if(data[i] <'0' || data[i] > '9'){
+			USART_SendString("invalid input.\r"); //data[1:end]
+			USART_SendString("invalid input.\rUsart closed, please try again later\r");
+			UART_Close();
+			break;
+		}
+		i++;
+	}
+	if(!data[i]){
+		u8 num = atoi(data);
+		if(num > LOWER_LIMIT && num < UPPER_LIMIT){
+			desiredTemp = num;
+			CLCD_voidMoveCursor(2, 10);
+			CLCD_voidSendString(data);
+		}
+		else{
+			USART_SendString("Temperature out of range.\r");
+			USART_SendString("Usart closed, please try again later\r");
+		}
+		UART_Close();
 	}
 }
